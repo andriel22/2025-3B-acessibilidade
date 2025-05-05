04b695e0a85b1e22cebad9575c4d7980786090a2
Andriel <141413682+andriel22@users.noreply.github.com>
2025-05-05 07:31:37 -0300

    Delete 2025-3b-acessibilidade-main directory

--- 2025-3b-acessibilidade-main/img/script.js
+++
@@ -1,8 +0,0 @@
-const  aumentaFonteBotao = document.getElementById('aumentar-fonte');
-
-let tamanhoFonteAtual = 1;
-
-aumentaFonteBotao.addEventListener('click', function(){
-    tamanhoFonteAtul += 0.1;
-    document.body.style.fontSize = `${tamanhoFonteAtual}rem`
-})
